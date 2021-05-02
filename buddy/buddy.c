d362dd0afdb677aa7adbc8f211ae9eba45b25cb9
Suhaib Ansari <suhaibansari@ku.edu>
2021-05-02 07:28:40 -0500

    one problem remains with recursion otherwise good

--- buddy/buddy.c
+++ buddy/buddy.c
@@ -74,18 +74,26 @@ page_t g_pages[(1<<MAX_ORDER)/PAGE_SIZE];
  **************************************************************************/
 int buddy(void *addr, int o){
 	//int addr1 = (unsigned long)(((void*)addr - (void*)g_memory) ^ (1 << o)) + (unsigned long)g_memory;
-	int num = (int)addr - ((int)g_memory)^ (1<<o);
+	int num = (int)addr - ((int)g_memory);
 	num = num/PAGE_SIZE;
 	int addr1 = BUDDY_ADDR(addr, o);
-	//addr1 = num + (1 << o);
+	//addr1 = num ^ (1 << o);
 	//addr1 = addr1 + (int)g_memory;
-	//printf("Buddy: %d\n", addr1);
+	// printf("Buddy: %d\n", addr1);
 	int p_addr1 = (int)((void*)addr1 - (void*)g_memory);
 	
 	p_addr1 = p_addr1/PAGE_SIZE;
+	p_addr1 = num + ((1<<o)/PAGE_SIZE);
 	//printf("addr: %d\n", p_addr1);
 	return p_addr1;
 
+}
+
+int leftbuddy(void *addr, int o){
+	int num = (int)addr - (int)g_memory;
+	num = num/PAGE_SIZE;
+	int p_addr1 = num - (((1 << o))/PAGE_SIZE);
+	return p_addr1;
 }
 
 void *split(int size, int index){
@@ -95,8 +103,8 @@ void *split(int size, int index){
 	for(int i = index; i < 21; i++){
 		if (list_empty(&free_area[i]) == 0)
 		{
-			//printf("big index: %d\n", index);
-			//printf("Index: %d\n", i);
+			// printf("big index: %d\n", index);
+			// printf("Index: %d\n", i);
 			page_t *ptr = list_entry(free_area[i].next, page_t, list); //gets the base address of the first list
 			int offset = ptr->startaddr;	
 			int addr = ptr ->addr;							  //the offset is necessary for if we're breaking apart something that doesn't start at 0
@@ -116,31 +124,35 @@ void *split(int size, int index){
 			if (i == index)
 			{
 				//printf("LINE 110\n");
-				g_pages[p_addr2].free = 0;
-				g_pages[p_addr2].free_index = i;
-				g_pages[p_addr1].free_index = i;
+				//g_pages[p_addr2].free = 0;
+				//g_pages[p_addr2].free_index = i;
+				//g_pages[p_addr1].free_index = i;
 				//printf("p_addr1: %d\n", p_addr1);
-				
+				struct list_head *pos1 = free_area[i].next;
 				struct list_head *pos = free_area[i].next->next;
-				page_t *ptr2 = list_entry(&pos, page_t, list);
+				page_t *ptr2 = list_entry(pos, page_t, list);
+				page_t *ting = list_entry(pos1, page_t, list);
 
+				ting -> free_index = i;
+				ting -> free = 0;
 				
 				list_del_init(&free_area[i]);
-				//printf("IN SPLIT: Page: %d, bud: %d, size: %d\n", p_addr2, p_addr1, i);
-				ptr2 = list_entry(&pos, page_t, list);
-				pos = g_pages[p_addr1].list.prev;
+				// printf("IN SPLIT: Page: %d, bud: %d, size: %d\n", p_addr2, p_addr1, i);
+				ptr2 = list_entry(pos, page_t, list);
+				//pos = g_pages[p_addr1].list.next;
 
 				
 				if (i < 20){
 					list_add(pos, &free_area[i]);
 					ptr2 = list_entry(free_area[i].prev, page_t, list);
+					ptr2 -> free_index = i;
 					//printf("Poggers 2: %d\n", ptr2 -> startaddr);
 				}
 				//printf("Yeet! Page: %d\n", p_addr2);
 				// printf("Buddy dump start: \n");
 				// buddy_dump();
 				// printf("Budy dump end: \n\n");
-				return g_pages[p_addr2].addr;							  //returns the list
+				return ting -> addr;							  //returns the address
 			}
 			else
 			{
@@ -183,44 +195,90 @@ void *split(int size, int index){
  * @param size the index inside the free area it would go
 */
 void merge(int page, int size){
-	//printf("Size: %d\n", size);
-	// printf("Addr: %d\n", g_pages[page].addr);
-	// printf("G-mem addr: %d\n", g_memory);
-	int bud = buddy(g_pages[page].addr, size-1);
+	int bud = buddy(g_pages[page].addr, size);
+	int lbud = leftbuddy(g_pages[page].addr, size);
+	
+	//printf("Page: %d, bud: %d, lbud: %d, size: %d\n", page, bud, lbud, size);
+	//printf("Buddy's free index: %d; buddy's free: %d\n\n", g_pages[bud].free_index, g_pages[bud].free);
+	
+	// printf("\nInside urge\n\n");
+	//  printf("8's buddy %d\n", buddy(PAGE_TO_ADDR(8), size));
+	// buddy_dump();
+	//printf("leaving urge\n\n");
 	
 	
-	//printf("Page: %d, bud: %d, size: %d\n", page, bud, size);
-	//printf("Buddy's free index: %d; buddy's free: %d\n", g_pages[bud].free_index, g_pages[bud].free);
+	
+	int yessir = 0;
+	int ham = g_pages[bud].free_index == size && g_pages[bud].free == 1;
+	int yam = 0;
+	if(lbud >= 0){
+		//printf("Left Buddy's free index: %d; left buddy's free: %d\n", g_pages[lbud].free_index, g_pages[lbud].free);
+		yam = g_pages[lbud].free_index == size && g_pages[lbud].free == 1;
+		
+	}
+	
+	yessir = ham || yam;
 	list_add(&g_pages[page].list, &free_area[size]);
-	int yessir = g_pages[bud].free_index == size && g_pages[bud].free == 1;
 
 	// struct list_head * pos;
 	// list_for_each(pos, &free_area[size]){
 	// 	page_t *ptr = list_entry(pos, page_t, list);
 	// 	if(ptr -> startaddr == bud){
+	// 		//printf("bud: %d\n", bud);
 	// 		yessir = 1;
 	// 	}
+	// 	if(ptr -> startaddr == lbud){
+	// 		yam = 1;
+	// 	}
 	// }
-	//printf("Inside purge\n\n");
-	//printf("8's buddy %d\n", buddy(PAGE_TO_ADDR(8), size));
+	
+	// printf("\nInside purge\n\n");
 	// buddy_dump();
 	// printf("leaving purge\n\n");
 	if(yessir == 1 && size < 20){
+		
 		//printf("innit\n");
 		struct list_head *temp = &g_pages[bud].list;
 		// __list_del(temp -> prev, temp -> next);
 		// __list_del(g_pages[page].list.prev, g_pages[page].list.next);
-		list_del_init(&g_pages[bud].list);
-		list_del_init(&g_pages[page].list);
+
+		// printf("Inside line 217\n\n");
+		// buddy_dump();
+		// printf("leaving...\n\n");
 		
-		g_pages[bud].free_index = 0;
+		if (yam == 1){
+			list_del_init(&g_pages[lbud].list);
+			list_del_init(&g_pages[page].list);
+			//list_add(&g_pages[lbud].list, &free_area[size]);
+			//g_pages[page].free_index = 0;
+			//g_pages[lbud].free_index++;
+			merge(lbud, size);
+		}
+
+		list_del_init(&g_pages[page].list);
+		// printf("Inside line 223\n\n");
+		// buddy_dump();
+		// printf("leaving...\n\n");
+
+		if (ham == 1){
+			list_del_init(&g_pages[bud].list);
+		}
+		
+		
+		
+
+		//g_pages[bud].free_index = 0;
 		g_pages[page].free_index++;
 		
 		// printf("Inside merge\n\n");
 		// buddy_dump();
 		// printf("leaving merge\n\n");
-		return merge(page, size+1);
-	}
+		merge(page, size+1);
+	} 
+}
+
+void nr_merge(){
+
 }
 
 int find(int index){
@@ -266,18 +324,7 @@ void buddy_init()
 	/* add the entire memory as a freeblock */
 	list_add(&g_pages[0].list, &free_area[MAX_ORDER]);
 
-	// for (i = 0; i < n_pages; i++)
-	// {
-	// 	list_add(&g_pages[i].list, &g_pages[0].list);
-	// }
-	//printf("Address of free area next vs address of g pages, %p, %p\n", free_area[MAX_ORDER].next, &g_pages[0].list);
-
-	// list_for_each(position, free_area[MAX_ORDER].next)
-	// {
-	// 	page_t *tempptr = list_entry(position, page_t, list);
-	// 	printf("Address: %d\n", tempptr->startaddr);
-	// 	printf("%p self %p ting %p bink\n", position, position->prev, position->next);
-	// }
+	
 	
 }
 
@@ -300,15 +347,10 @@ void *buddy_alloc(int size)
 	/* TODO: IMPLEMENT THIS FUNCTION */
 	for(int i = 12; i < 21; i++){
 		int num = 1 << i;
-		// printf("size: %d\n", size);
-		// printf("%d\n", num);
 		if(size <= num){
 			int index = find(i);
 			if(index == 0){
-				//find the page address to set this to not free
-				//break off the correct size piece from memory
-
-				//printf("wrong place\n");
+				
 				struct list_head *temporary = free_area[i].next;
 				page_t *ptr = list_entry(free_area[i].next, page_t, list);
 				int addr = ptr -> addr;
@@ -320,19 +362,9 @@ void *buddy_alloc(int size)
 				
 				return ptr -> addr;
 			} else {
-				//printf("we in it: %d\n", i);
+				
 				void* map = split(num, i);
-				//printf("sir\n");
-				// for(int i = 12; i < 21; i++){
-				// 	if(find(i) == 0){
-				// 		struct list_head *pointer;
-				// 		list_for_each(pointer, free_area[i].next){
-				// 			printf("pointer: %p\n", pointer);
-				// 		}
-				// 		printf("Yeet %d\n", i);
-				// 	}
-				// }
-				// printf("here?\n");
+				
 				return map;
 			}
 		}
@@ -356,18 +388,14 @@ void buddy_free(void *addr)
 	/* TODO: IMPLEMENT THIS FUNCTION */
 	int page = (void*)addr - (void*)g_memory;
 	page = page >> MIN_ORDER;
-	//printf("This page: %d\n", page);
+
 	g_pages[page].free = 1;
-
-	// for(int i = 0; i < 256; i++){
-	// 	int num = buddy(PAGE_TO_ADDR(i), 15);
-	// 	int num2 = buddy(PAGE_TO_ADDR(i), 16);
-
-	// 	printf("Index %d, Real address %d, First num %d, second num %d\n", i, PAGE_TO_ADDR(i),num, num2);
-	// }
-	
+	//list_add(&g_pages[page].list, &free_area[g_pages[page].free_index]);
 	merge(page, g_pages[page].free_index);
-	//printf("done merging\n");
+	
+	
+	
+
 }
 
 /**
